da0ba7dbfd28ed7b41a083f50b68302d48ac5ed3
Users <lana.chebotareva.95@mail.ru>
2018-12-04 15:22:06 +0300

    add sort

--- src/main/java/strategy/SorterA.java
+++ src/main/java/strategy/SorterA.java
@@ -9,6 +9,8 @@ public class SorterA implements Sorter {
 
     @Override
     public void sort(List<Pupil> pupils) {
-        pupils.sort((o1, o2) -> Pupils.calculateAverageMark(o1) > Pupils.calculateAverageMark(o2) ? -1: 1);
+       // pupils.sort((o1, o2) -> Pupils.calculateAverageMark(o1) > Pupils.calculateAverageMark(o2) ? -1: 1);
+        pupils.sort((o1, o2) -> Pupils.calculateAverageMark(o1) < Pupils.calculateAverageMark(o2)? -1:1);
+
     }
 }
